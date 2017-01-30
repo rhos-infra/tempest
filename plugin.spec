redhat-openstack/infrared

Tempest input files fix
 * Removed deployer-input-method. Now all files should
go from the local machine
 * Added support of the relative paths
 * Added documentation

RHOSINFRA-662

Change-Id: Icefc1523be300f3c6cf631e2569410ed6d6a7250

plugins/tempest/plugin.spec
@@ -35,25 +35,16 @@ subparsers:
                           The setup type for tests
                           __LISTYAMLS__
                       default: git
-                  deployer-input-method:
-                      type: Value
-                      help: |
-                          Possible values:
-                             - copy: The deployer-input specified by the '--deployer-input-file' argument will be copied from the host station.
-                             - local: The deployer-input file to use from the tester folder on tester station.
-                      choices: [copy, local]
-                      default: copy
                   deployer-input-file:
                       type: Value
                       help: |
-                          Required when the 'deployer-input-method' option is set.
-                          Specifies the deployer-input file location. When  deployer-input-method == 'copy' the absolute path to the file should be specified.
-                          When  deployer-input-method == 'local' the tempest-dir relative path should be specified (e.g. etc/deployer-config-icehouse.conf).
+                          The deployer input file absolute or relative path.
+                          By default will try to use  ~/deployer-input-file.conf on the tester station.
                   openstackrc:
                       type: Value
                       help: |
-                          The full path to the openstackrc file.
-                          When empty, will search active profile for 'keystonerc' file"
+                          The full path or relative path to the openstackrc file.
+                          When empty, InfraRed will search active profile for the 'keystonerc' file and use it.
                   config-options:
                        type: KeyValueList
                        help: |

rhos-infra/tempest

plugin.spec
@@ -35,25 +35,16 @@ subparsers:
                           The setup type for tests
                           __LISTYAMLS__
                       default: git
-                  deployer-input-method:
-                      type: Value
-                      help: |
-                          Possible values:
-                             - copy: The deployer-input specified by the '--deployer-input-file' argument will be copied from the host station.
-                             - local: The deployer-input file to use from the tester folder on tester station.
-                      choices: [copy, local]
-                      default: copy
                   deployer-input-file:
                       type: Value
                       help: |
-                          Required when the 'deployer-input-method' option is set.
-                          Specifies the deployer-input file location. When  deployer-input-method == 'copy' the absolute path to the file should be specified.
-                          When  deployer-input-method == 'local' the tempest-dir relative path should be specified (e.g. etc/deployer-config-icehouse.conf).
+                          The deployer input file absolute or relative path.
+                          By default will try to use the 'deployer-input-file.conf' file from active workspace folder.
                   openstackrc:
                       type: Value
                       help: |
-                          The full path to the openstackrc file.
-                          When empty, will search active workspace for 'keystonerc' file"
+                          The full path or relative path to the openstackrc file.
+                          When empty, InfraRed will search active workspace for the 'keystonerc' file and use it.
                   config-options:
                        type: IniType
                        action: append